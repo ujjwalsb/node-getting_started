b3a38e406204b907dfd3b570da830a92e062d0ef
ujjwalcrypto <ujjwalsingh15@gmail.com>
2018-11-09 16:45:14 +0530

    first node server for hello world

--- myfirst.js
+++ myfirst.js
@@ -4,3 +4,4 @@ http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end('Hello World!');
 }).listen(8080);
+console.log('Open browser and enter http://127.0.0.1:8080')
