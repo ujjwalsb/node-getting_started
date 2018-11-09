c016dc2b74becfaf51fd890a1ca39f9888b45413
ujjwalcrypto <ujjwalsingh15@gmail.com>
2018-11-09 17:22:56 +0530

    added comments on including modules

--- 3_includingmodule.js
+++ 3_includingmodule.js
@@ -3,9 +3,10 @@ var http = require('http');     // to include a module, use require() function w
 // http --> To make Node.js act as an HTTP server  
 var dt = require('./2_myfirstmodule');        // Includes the module file from 2_myfirstmodule.js  
 
+//create a server object:
 http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write('The current date and time is: ' + dt.myDateTime());
-    res.end();
-}).listen(8080);
+    res.write('The current date and time is: ' + dt.myDateTime());      // write a response to client
+    res.end();      // end of response
+}).listen(8080);        // server object listens on port 8080.
 console.log('Open browser and enter http://127.0.0.1:8080')
